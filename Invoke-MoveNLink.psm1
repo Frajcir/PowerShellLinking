65439cfa42426fc841274fb0a27181106d79b113
Kyle Krajcir <kekrajcir@gmail.com>
2020-11-21 00:12:14 -0500

    Update Invoke-MoveNLink.psm1
    Added features

--- Invoke-MoveNLink.psm1
+++ Invoke-MoveNLink.psm1
@@ -1,47 +1,84 @@
 
 function Invoke-MoveNLink {
     param (
-        # Source
-        [Parameter(Mandatory=$true)]
-        [string]
-        $SourceFilePath,
-        # Destination
-        [Parameter(Mandatory=$true)]
-        [string]
-        $DestinationFilePath,
-        # MaxLastWriteTime
-        [Parameter(Mandatory=$true)]
-        [datetime]
-        $MaxLastWriteTime
+        [Parameter(Mandatory=$true,ValueFromPipeline)][string]$Path,
+        [Parameter(Mandatory=$true)][string]$Destination,
+        [Parameter(Mandatory=$true)][datetime]$MaxLastWriteTime,
+        [Parameter(Mandatory=$False)][string[]]$ExcludeFileExtensions,
+        [Parameter(Mandatory=$False)][switch]$Recurse,
+        [Parameter(Mandatory=$False)][switch]$WhatIf
     )
     
-    if (-not (Test-Path -Path $SourceFilePath)) {
-        Write-Error "Invalid source path"
+    # VALIDATE SOURCE PATH
+    try {
+        $SourceItem = Get-Item -Path $Path -ErrorAction Stop
+    }
+    catch {
+        Write-Error -ErrorRecord $_
+        break
+    }
+    Write-Verbose "Path found $($SourceItem.FullName)"
+    if ( -not ($SourceItem.PSIsContainer) ) {
+        Write-Error "Source path is not a container"
         return
     }
-    if (-not (Test-Path -Path $DestinationFilePath)) {
-        New-Item -ItemType Directory -Path $DestinationFilePath -ErrorAction Stop
+
+    # VALIDATE DESTINATION PATH
+    if (-not (Test-Path -Path $Destination)) {
+        $ResultDestinationDirectory = New-Item -ItemType Directory -Path $Destination -ErrorAction Stop
+        Write-Verbose "Destination directory created $($ResultDestinationDirectory.FullName)"
     }
     else {
         Write-Verbose "Destination directory already exists"
     }
 
-    $SourceFiles = Get-ChildItem -Path $SourceFilePath -File -Recurse -Force | Where-Object -Property LastWriteTime -lt $MaxLastWriteTime
-    $SourceDirectories = Get-ChildItem -Path $SourceFilePath -Directory -Recurse
+    # PARAMATERS FOR SPLATTING TO GET-CHILDITEM FOR SOURCE PATH
+    $GCIparam = @{Path=$SourceItem.FullName;Recurse=$false;Exclude=""}
+    if ($Recurse.IsPresent) {
+        $GCIparam.Recurse = $true
+    }
+    if ($ExcludeFileExtensions) {
+        for ($i = 0; $i -lt $ExcludeFileExtensions.Count; $i++) {
+            if ($ExcludeFileExtensions[$i][0] -ne "*") {
+                $ExcludeFileExtensions[$i] = $ExcludeFileExtensions[$i].Insert(0,"*")
+            }
+            if ($ExcludeFileExtensions[$i][1] -ne ".") {
+                $ExcludeFileExtensions[$i] = $ExcludeFileExtensions[$i].Insert(1,".")
+            }
+        }
+        $GCIparam.Exclude = $ExcludeFileExtensions
+    }
+    
+    # GET SOURCE FILES AND DIRECTORIES
+    $AllChildItems = Get-ChildItem @GCIparam
+    $SourceFiles = $AllChildItems | Where-Object  {$_.LastWriteTime -lt $MaxLastWriteTime -and $_.PSIsContainer -EQ $False -and $_.Attributes -notmatch "ReparsePoint"}
+    $SourceDirectories = $AllChildItems | Where-Object {$_.PSIsContainer -EQ $True}
+    Write-Verbose "Found in source $($SourceDirectories.Count) directories and $($SourceFiles.Count) files"
 
-    #Create all directories
-    $SourceDirectories | Foreach-Object {
-        $d = $DestinationFilePath + $_.FullName.Substring($SourceFilePath.Length)
+    # WHATIF PARAMETER FOR SPLATTING TO IMPACTFUL CMDLETS
+    $WhatIfParam = @{WhatIf=$false}
+    if ($WhatIf.IsPresent) {
+        $WhatifParam.WhatIf = $true
+    }
+
+    # CREATE DESTINATION DIRECTORIES AND SUBDIRECTORIES
+    $ResultDirectories = $SourceDirectories | Foreach-Object {
+        $d = $Destination + $_.FullName.Substring($Path.Length)
         if (-not (Test-Path -Path $d)) {
-            New-Item -ItemType Directory -Path $d
+            New-Item -ItemType Directory -Path $d @WhatIfParam
         }
     }
-    # $SourceFiles | format-table
+    Write-Verbose "Created $($ResultDirectories.Count) directories in the destination directory"
 
-    $SourceFiles | Foreach-Object {
-        $d = $DestinationFilePath + $_.FullName.Substring($SourceFilePath.Length)
-        $f = $_.CopyTo($d,$true)
-        # New-Item -ItemType SymbolicLink -Path
+    # COPY SOURCE FILES TO DESTINATION AND CREATE SYMBOLIC LINK IN SOURCE
+    $ResultSymLinks = $SourceFiles | Foreach-Object {
+        $d = $Destination + $_.FullName.Substring($Path.Length)
+        # $f = $_.CopyTo($d,$true)
+        Copy-Item -Path $_.FullName -Destination $d @WhatifParam | Out-Null
+        # New-Item -ItemType SymbolicLink -Path $_.FullName -Target $f.FullName -Force @WhatifParam
+        New-Item -ItemType SymbolicLink -Path $_.FullName -Target $d -Force @WhatifParam
     }
+
+    Write-Output "Copied and created symbolic links for $($ResultSymLinks.Count) files in the source directory"
 
 }
